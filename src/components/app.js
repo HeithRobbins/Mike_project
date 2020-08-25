e605f8e6bb2d334711dec494a0bba4b7535afa12
Heith <Heith.Robbins@gmail.com>
2020-08-25 09:41:12 -0600

    first commit

---
+++ src/components/app.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <h1>this is the start of mikes website in one day</h1>
+      </div>
+    );
+  }
+}
